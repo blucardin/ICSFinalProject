d5f46aec8b0cc0bb76a756c90462f13a6f62b339
Sam Cardin <blucardin@gmail.com>
2022-12-08 11:30:16 -0500

    Add calucaltor file and method

---
+++ src/calculator.java
@@ -0,0 +1,9 @@
+public class calculator {
+    public static double eval(String expression) {
+        return 0.1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(eval("1+1"));
+    }
+}
